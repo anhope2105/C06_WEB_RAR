a4795abe0f0cdbc3b8f667ac736ae20d45e79403
Duong Tien Dat <duongtiendat3103@gmail.com>
2024-01-22 23:34:57 +0700

    PHWQT_15 img

---
+++ src/html/bai-viet/image.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $(".btn-img").on('click', handleImage);
+
+    function handleImage() {
+        document.getElementById("phwqt_15_img").click()
+    }
+});
+
+const handleFileChange = e => {
+    console.log(e);
+}
+
