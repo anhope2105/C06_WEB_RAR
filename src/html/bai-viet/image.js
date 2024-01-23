a0a314772f144cf555e275c0aee02a59ec41e69d
datdt <duongtientiendat3103@gmail.com>
2024-01-23 09:56:13 +0700

    PHWQT_15 lam button chon anh v1

--- src/html/bai-viet/image.js
+++ src/html/bai-viet/image.js
@@ -1,12 +1,114 @@
-$(document).ready(function() {
-    $(".btn-img").on('click', handleImage);
+// $(document).ready(function () {
+//   $(".btn-img").on("click", handleImage);
 
-    function handleImage() {
-        document.getElementById("phwqt_15_img").click()
+//   const uploadedFile = {
+//     raw: null,
+//     name: "",
+//     size: 0,
+//     type: "",
+//     fileExtention: "",
+//     url: "",
+//     isUploaded: false,
+//   };
+
+//   function handleImage() {
+//     document.getElementById("phwqt_15_img").click();
+//   }
+
+//   // Đặt lắng nghe sự kiện change cho phần tử phwqt_15_img
+//   $("#phwqt_15_img").on("change", handleFileChange);
+
+//   function handleFileChange(e) {
+//     const filesList = e.target.files;
+//     console.log("filesList: ", filesList);
+//     if (filesList && filesList[0]) {
+//       const raw = filesList[0];
+
+//       const fileSize = Math.round((raw.size / 1024 / 1024) * 100) / 100;
+
+//       const fileExtention = raw.name.split(".").pop();
+
+//       const rawFileName = raw.name;
+//       console.log("base64", e.target?.result);
+//       console.log("rawFileName", rawFileName);
+//       const reader = new FileReader();
+//       reader.addEventListener(
+//         "load",
+//         (e) => {
+//           const base64 = e.target?.result;
+//           // Set file data
+//           uploadedFile = {
+//             raw,
+//             name: rawFileName,
+//             size: fileSize,
+//             type: raw.type,
+//             fileExtention: fileExtention,
+//             url: e.target?.result,
+//             isUploaded: true,
+//           };
+//           //   syncValue.value = base64.split(",").pop();
+//           //   syncFileName.value = rawFileName;
+//           sendDataToParent();
+//         },
+//         false
+//       );
+//     }
+//   }
+// });
+$(document).ready(function () {
+  $(".btn-img").on("click", handleImage);
+
+  let uploadedFile = {
+    raw: null,
+    name: "",
+    size: 0,
+    type: "",
+    fileExtension: "",
+    url: "",
+    isUploaded: false,
+  };
+
+  function handleImage() {
+    document.getElementById("phwqt_15_img").click();
+  }
+
+  // Đặt lắng nghe sự kiện change cho phần tử phwqt_15_img
+  $("#phwqt_15_img").on("change", handleFileChange);
+
+  function handleFileChange(e) {
+    const filesList = e.target.files;
+    if (filesList && filesList[0]) {
+      const raw = filesList[0];
+
+      const fileSize = Math.round((raw.size / 1024 / 1024) * 100) / 100;
+      const fileExtension = raw.name.split(".").pop();
+      const rawFileName = raw.name;
+
+      const reader = new FileReader();
+
+      reader.addEventListener("load", (event) => {
+        const base64 = event.target.result;
+        // Set file data
+        uploadedFile = {
+          raw,
+          name: rawFileName,
+          size: fileSize,
+          type: raw.type,
+          fileExtension,
+          url: base64,
+          isUploaded: true,
+        };
+        // Hiển thị ảnh
+        $("#uploadedImage").attr("src", base64).show();
+
+        // Hiển thị tên ảnh
+        $("#imageName")
+          .text(rawFileName)
+          .show();
+      });
+
+      // Bắt đầu đọc tệp như là URL dữ liệu
+      reader.readAsDataURL(raw);
     }
+  }
 });
-
-const handleFileChange = e => {
-    console.log(e);
-}
-
--- src/html/bai-viet/index.js
+++ src/html/bai-viet/index.js
@@ -16,22 +16,8 @@ Array.from(forms).forEach((form) => {
   );
 });
 
-// $(document).ready(function () {
-//   $("table th").css("background", "#FAFAFA");
-
-//   $(document).on("click", ".btn-group", function (event) {
-//     var target = $(event.target);
-//     console.log(target);
-//     if (target.is("button")) {
-//       // Toggle the 'border-black' class on the button
-//       $(".ti-dots-vertical")
-//         .addClass("border-black")
-//         .removeClass("border-default");
-//     } else {
-//       // Remove 'border-black' class and restore 'border-default'
-//       $(".ti-dots-vertical")
-//         .removeClass("border-black")
-//         .addClass("border-default");
-//     }
-//   });
-// });
+$(document).ready(function () {
+  $("#btn-back-detail").on("click", function () {
+    console.log("vào");
+  });
+});
